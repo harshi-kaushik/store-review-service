137d65fd1e6d64a631ce27b7c5788e6ac8b721f2
harshi-kaushik <harshitakaushik11099@gmail.com>
2025-07-10 23:05:32 +0530

    Create Login.tsx

---
+++ web-app/src/pages/Login.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import API from '../api/api';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    try {
+      const res = await API.post('/auth/login', { email, password });
+      localStorage.setItem('token', res.data.token);
+      navigate('/stores');
+    } catch (err) {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
