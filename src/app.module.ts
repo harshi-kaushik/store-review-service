76d1c8f25e7917bbf8d515f8575e497b499d7c2c
harshi-kaushik <harshitakaushik11099@gmail.com>
2025-07-10 22:55:30 +0530

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'your_db_user',
+      password: 'your_db_password',
+      database: 'rating_platform',
+      autoLoadEntities: true,
+      synchronize: true, // use false in production
+    }),
+  ],
+})
+export class AppModule {}
